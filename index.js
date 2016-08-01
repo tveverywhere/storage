32c01fa49c06b3f767b4ec7087a2c4dac08efa7c
Anuj Pandey <a@e10.in>
2016-08-01 20:15:47 +0530

    Default to Azure

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var _storageFactory=function(d){
     var _supported=['amazon','azure','ftp'];
     if(!d){ return function(){ throw new Error('no storage record found');};}
-    if(!d.Provider){ return function(){ throw new Error('no storage provider sepcified, e.g Amazon, Azure, Ftp');};}
-    if(_supported.indexOf(d.Provider.toLowerCase())==-1){return function(){ throw new Error('storage provider not supported, expected Amazon, Azure or Ftp.');};}
+    if(!d.Provider || _supported.indexOf(d.Provider.toLowerCase())==-1){ return require('./storage-azure');}
+    //if(){return function(){ throw new Error('storage provider not supported, expected Amazon, Azure or Ftp.');};}
     return require('./storage-'+d.Provider.toLowerCase());
 }
 
